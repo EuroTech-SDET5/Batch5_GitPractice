f78dd86ca4c23d02c419136f2e62c7b870dac9a1
Talha <talha43gs@gmail.com>
2022-10-12 19:31:27 +0100

    SeleniumTest.java file Updated

--- src/AutomationTool/SeleniumTest.java
+++ src/AutomationTool/SeleniumTest.java
@@ -3,5 +3,6 @@ package AutomationTool;
 public class SeleniumTest {
     public static void main(String[] args) {
         System.out.println("TEST1");
+        System.out.println("TEST2");
     }
 }
